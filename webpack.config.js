4c25d8f96d8cbb90215b5ff8bb8311791a474c07
dudashh <dudash.bohdan@student.uzhnu.edu.ua>
2024-11-05 18:36:59 +0200

    first

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,42 @@
 import HtmlWebpackPlugin from 'html-webpack-plugin';
- 
-import { resolve as _resolve, join } from 'path';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
 
- 
-export const entry = './src/index.js';
-export const mode = 'development';
-export const output = {
-  // eslint-disable-next-line no-undef
-  path: _resolve(__dirname, './dist'),
-  filename: 'index_bundle.js',
-};
-export const target = 'web';
-export const devServer = {
-  port: 5000,
-  static: {
-    // eslint-disable-next-line no-undef
-    directory: join(__dirname, 'public'),
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default {
+  entry: './src/index.js',
+  mode: 'development',
+  output: {
+    path: join(__dirname, './dist'),
+    filename: 'index_bundle.js',
   },
-  open: true,
-  hot: true,
-  liveReload: true,
-};
-export const resolve = {
-  extensions: ['.js', '.jsx', '.json'],
-};
-export const module = {
-  rules: [
-    {
-      test: /\.(js|jsx)$/,
-      exclude: /node_modules/,
-      use: 'babel-loader',
+  target: 'web',
+  devServer: {
+    port: 5000,
+    static: {
+      directory: join(__dirname, 'public'),
     },
+    open: true,
+    hot: true,
+    liveReload: true,
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: join(__dirname, 'public', 'index.html'),
+    }),
   ],
 };
-export const plugins = [
-  new HtmlWebpackPlugin({
-    // eslint-disable-next-line no-undef
-    template: join(__dirname, 'public', 'index.html'),
-  }),
-];
